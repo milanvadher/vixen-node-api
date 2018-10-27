03b1ff9ec773b78b44d22a0d0b00add5ef898bd8
Milan Vadher <milanvadher1996@gmail.com>
2018-10-27 11:54:40 +0530

    add excecutable file for run server

---
+++ ForFreshMachine.bat
@@ -0,0 +1,17 @@
+title Vixen Light Controller Server
+
+echo Installing Dependency ...
+
+::Install node Dependency
+npm install
+
+echo Dependency installed successfully.
+
+echo Installing PM2 ...
+
+::Install PM2 for run node server
+npm install -g pm2
+
+echo PM2 installed successfully.
+
+echo Everything is installed successfully Now run "doubleClick" file.
---
+++ doubleClick.bat
@@ -0,0 +1,8 @@
+echo Starting the server.
+
+::Start server
+pm2 start index.js
+
+echo Server started successfully.
+
+echo Do not close this Termonal / CMD.
